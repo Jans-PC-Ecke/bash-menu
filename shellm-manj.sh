1dc35162dbed0b38d9ebca803d3830a455165850
Jan <112738312+Jans-PC-Ecke@users.noreply.github.com>
2022-09-10 21:12:35 +0200

    Add files via upload

---
+++ shellm-manj.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+trap '' 2
+while true
+do
+
+	clear
+	echo ""
+	echo ""
+	echo ""
+	date +%a,%F---%T
+	echo ""
+	echo "      			 ===================================================================="
+	echo "	  	            			    >> SHELL MENU <<"
+	echo "       			 ===================================================================="
+	echo ""
+	echo ""
+	echo "                                                          BASH"
+	echo "					                  ____"
+	echo ""
+	echo ""
+	echo "	1 = Bashtop		2 = Cmatrix		3 = HTOP"
+	echo ""
+	echo "	4 = MC			5 = Neofetch		6 = Uhrzeit"
+	echo ""
+	echo " 	7 = Hans		8 = DNS			9 = Update"
+	echo ""
+	echo " 	10 = IP-Addresse	11 = Herunterfahren"
+	echo ""
+	echo "================================="
+	echo ""
+	echo "	q = Zurück"
+	
+	echo -e "\n"
+echo -e "  		===>> Option auswählen: \c"
+		read answer
+		case "$answer" in
+	
+		1) bashtop;;
+	    2) cmatrix -a -b -s -Cred;;
+		3) htop;;
+		4) sudo mc;;
+		5) neofetch;;
+		6) tty-clock -c -C1 -S -s;;
+		7) ssh hans@192.168.1.117;;
+		8) ssh raspberry@192.168.1.89;;
+		9) ./update.sh;;
+		10) ip addr;;
+		11) sudo shutdown -n;;
+	q) exit;;
+	esac
+	read input
+done
